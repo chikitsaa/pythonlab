0c76cea5a0090ff6da8d7af882c4f0135b0ab67c
chikitsaa <43951051+chikitsaa@users.noreply.github.com>
2018-10-27 01:34:27 +0530

    Add files via upload

---
+++ Details of student.py
@@ -0,0 +1,21 @@
+print("-"*100)
+Name=input("Enter user name-")
+Roll_number=input("Enter user roll no-")
+GR_number=input("Enter user GR no-")
+Branch=input("Enter user branch-")
+Year=input("Enter user year-")
+print("-"*1000)
+ 
+Name=[]
+Name.append(input("Enter user name-"))
+Roll_number=[]
+Roll_number.append(input("Enter user roll no-"))
+GR_number=[]
+GR_number.append(input("Enter user GR no-"))
+Branch=[]
+Branch.append(input("Enter user branch-"))
+Year=[]
+Year.append(input("Enter user year-"))
+
+
+print("-"*1000)
